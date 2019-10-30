41357c52cf7d370a10e7a61087ed93452f5aeaa4
Aman Sultan Baig <41924807+AmanSultanBaig@users.noreply.github.com>
2019-10-30 21:04:07 +0500

    Add files via upload

---
+++ third-react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MyComponents from './components/Class-Components'
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponents value="Class Component!"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ third-react-app/src/components/Class-Components.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// Note: in react your functional / class component's name must
+// start with capital letter otherwise react will occurs an error
+class MyComponents extends Component {
+    render() {
+      return <h1>Hello from, {this.props.value}</h1>;
+    }
+  }
+  
+
+export default MyComponents
