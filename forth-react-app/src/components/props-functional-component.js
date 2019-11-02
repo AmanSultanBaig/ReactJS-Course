92dee4620d815da91b587788d73e2000025112db
Aman Sultan Baig <41924807+AmanSultanBaig@users.noreply.github.com>
2019-11-02 20:35:21 +0500

    Update props-functional-component.js

--- forth-react-app/src/components/props-functional-component.js
+++ forth-react-app/src/components/props-functional-component.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+// props are use to pass dynamic data to react component
+// props is simply properties 
 const FunctionalProps = (props) => {
     return <h1>Hello {props.message}</h1>
 }
