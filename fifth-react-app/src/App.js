89c45bdc43834a351e44bc6b8eda01439a623836
Aman Sultan Baig <41924807+AmanSultanBaig@users.noreply.github.com>
2019-11-03 17:14:31 +0500

    Add files via upload

---
+++ fifth-react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { SetState } from './components/SetState-Component'
+
+function App() {
+  return (
+    <div className="App">
+      <SetState/>
+    </div>
+  );
+}
+
+export default App;
